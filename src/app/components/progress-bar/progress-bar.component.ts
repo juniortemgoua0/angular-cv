0634dba4c944f052156aceeada96d2f9d058be3a
juniortemgoua0 <juniortemgoua0@gmail.com>
2022-03-05 07:31:20 +0100

    mise a jour du progress-bar

--- src/app/components/progress-bar/progress-bar.component.ts
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
@@ -9,9 +9,11 @@ export class ProgressBarComponent implements OnInit {
 
   @Input() level: number = 0
   @Input() colors!: string
-  @Output() onDispachColor:EventEmitter<string> = new EventEmitter<string>()
+  @Output() onDispachColor: EventEmitter<string> = new EventEmitter<string>()
+  @ViewChild('switcher_bar') switcherBar!: ElementRef<HTMLDivElement>
+  @ViewChild('switcher_circle') switcherCircle!: ElementRef<HTMLSpanElement>
 
-  progressLevel : number = 0
+  progressLevel: number = 0
   color: string = 'red'
 
   constructor() {
@@ -19,29 +21,29 @@ export class ProgressBarComponent implements OnInit {
 
   ngOnInit(): void {
     this.level = this.level >= 0 && this.level <= 100 ? this.level : 0
-    let timer = setInterval(()=>{
-      if(this.progressLevel<this.level){
+    let timer = setInterval(() => {
+      if (this.progressLevel < this.level) {
         this.progressLevel = this.progressLevel + 1
-        this.getColor()
-      }else{
+        this.updateView()
+      } else {
         clearInterval(timer)
       }
-    },20)
+    }, 20)
   }
 
-  getColor() {
+  updateView() {
     if (this.progressLevel >= 0 && this.progressLevel < 10) {
       this.color = "red"
-    }
-    else if (this.progressLevel >= 10 && this.progressLevel < 50) {
+    } else if (this.progressLevel >= 10 && this.progressLevel < 50) {
       this.color = "yellow"
-    }
-    else if (this.progressLevel >= 50 && this.progressLevel < 70) {
+    } else if (this.progressLevel >= 50 && this.progressLevel < 70) {
       this.color = "green"
-    }
-    else if (this.progressLevel >= 70 && this.progressLevel <= 100) {
+    } else if (this.progressLevel >= 70 && this.progressLevel <= 100) {
       this.color = "#1376ba"
     }
+    this.switcherBar.nativeElement.style.backgroundColor = this.color
+    this.switcherBar.nativeElement.style.width = this.progressLevel - 3 + '%'
+    this.switcherCircle.nativeElement.style.backgroundColor = this.color
     this.onDispachColor.emit(this.color)
   }
 
